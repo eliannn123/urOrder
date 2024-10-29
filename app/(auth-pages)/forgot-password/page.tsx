5001fb40ed73a557062382b7c77ac33cbe7c14e6
Elian Fernández <eliann2110@gmail.com>
2024-10-29 17:30:28 -0600

    Update page.tsx

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -4,12 +4,12 @@ import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
-export default function ForgotPassword({
-  searchParams,
-}: {
-  searchParams: Record<string, string> | null;
-}) {  
+export default function ForgotPassword() {
+  const searchParams = useSearchParams();
+  const message = searchParams.get("message") as unknown as Message;
+
   return (
     <>
       <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
@@ -28,7 +28,7 @@ export default function ForgotPassword({
           <SubmitButton formAction={forgotPasswordAction}>
             Reset Password
           </SubmitButton>
-          <FormMessage message={searchParams as Message} />
+          <FormMessage message={message} />
         </div>
       </form>
     </>
